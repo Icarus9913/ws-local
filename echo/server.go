1abba8b2e4d4a55305b2cdc4bea38e0b247ad6cd
Icarus Wu <icaruswu66@qq.com>
2026-01-22 17:19:26 +0800

    ..
    Signed-off-by: Icarus Wu <icaruswu66@qq.com>

---
+++ echo/server.go
@@ -0,0 +1,41 @@
+//go:build ignore
+// +build ignore
+
+package main
+
+import (
+	"flag"
+	"log"
+	"net/http"
+
+	"github.com/gorilla/websocket"
+)
+
+var addr = flag.String("addr", "localhost:8080", "http service address")
+
+var upgrader = websocket.Upgrader{} // use default options
+
+func echo(w http.ResponseWriter, r *http.Request) {
+	c, err := upgrader.Upgrade(w, r, nil)
+	if err != nil {
+		log.Print("upgrade:", err)
+		return
+	}
+	defer c.Close()
+	for {
+		_, message, err := c.ReadMessage()
+		if err != nil {
+			log.Println("read:", err)
+			break
+		}
+		log.Printf("recv: %s", message)
+		c.WriteMessage(websocket.TextMessage, []byte("Echo: "+string(message)))
+	}
+}
+
+func main() {
+	flag.Parse()
+	log.SetFlags(0)
+	http.HandleFunc("/echo", echo)
+	log.Fatal(http.ListenAndServe(*addr, nil))
+}
